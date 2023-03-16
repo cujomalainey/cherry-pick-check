e8e1e4fd577dbe9496d488f0eeb5dbdd06029d2a
Curtis Malainey <cujomalainey@chromium.org>
2023-03-16 16:24:00 -0700

    Pile of fixups from reviews

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,63 +7,74 @@ GREEN='\033[0;32m'
 NC='\033[0m'
 REGEX_REF='^\(cherry picked from commit ([a-f0-9]{40})\)$'
 
-do_reference_check() {
-  if ! [[ $(git log --format=%B "$line^!" | grep -E "$REGEX_REF") =~ $REGEX_REF ]]; then
-    echo -e "${RED}FAIL${NC}" >> "$GITHUB_OUTPUT"
-    dirty=1
-    echo "  Cannot find reference in commit message" >> "$GITHUB_OUTPUT"
-    echo "  Hint: use -x with git cherry-pick" >> "$GITHUB_OUTPUT"
-    return;
-  fi
+die()
+{
+  printf '%s ERROR: ' "$0" >> "$GITHUB_OUTPUT"
+  # We want die() to be usable exactly like printf
+  # shellcheck disable=SC2059
+  printf "$@" >> "$GITHUB_OUTPUT"
+  exit 1
+}
 
-  if ! eval "$(git branch --contains "${BASH_REMATCH[1]}" "$check_branch" 2>/dev/null)"; then
+do_reference_check() {
+  local commit=$1
+  local check_branch=$2
+  [[ $(git log --format=%B "$commit^!" | grep -E "$REGEX_REF") =~ $REGEX_REF ]] || {
     echo -e "${RED}FAIL${NC}" >> "$GITHUB_OUTPUT"
-    dirty=1
-    echo "  Cannot find ${BASH_REMATCH[1]} from $line in $check_branch" >> "$GITHUB_OUTPUT"
+    printf "  Cannot find reference in commit messag\n  Hint: use -x with git cherry-pick\n" >> "$GITHUB_OUTPUT"
+    echo 1;
     return;
-  fi
+  }
+
+  # check reference sha actually exists in parent
+  test -z $(git branch --contains "${BASH_REMATCH[1]}" "$check_branch" 2>/dev/null) || {
+    echo -e "${RED}FAIL${NC}" >> "$GITHUB_OUTPUT"
+    echo "  Cannot find ${BASH_REMATCH[1]} from $commit in $check_branch" >> "$GITHUB_OUTPUT"
+    echo 1;
+    return;
+  }
   echo -e "${GREEN}OK${NC}" >> "$GITHUB_OUTPUT"
+  echo 0;
 }
 
 do_subject_check() {
-  summary=$(git log --format=%s "$line^!")
+  local commit=$1
+  local check_branch=$2
+  summary=$(git log --format=%s "$commit^!")
   if (($(git log --format=%s "$check_branch" | grep -Fxc "$summary") == 0)); then
     echo -e "${RED}FAIL${NC}" >> "$GITHUB_OUTPUT"
-    dirty=1
     echo "  Cannot find $summary in $check_branch" >> "$GITHUB_OUTPUT"
+    echo 1;
     return;
   fi
   echo -e "${GREEN}OK${NC}" >> "$GITHUB_OUTPUT"
+  echo 0;
 }
 
 main() {
-  if [[ -z "${GITHUB_HEAD_REF}" ]]; then
-    echo "Must be run on pull_request or pull_request_target triggers only!" >> "$GITHUB_OUTPUT"
-    exit 1
-  fi
+  local DIRTY_PR=0
+  local check_branch=$2
+  local ret=0
+  local ref_check=$1
 
-  if [ "$GITHUB_BASE_REF" = "$2" ]; then
-    echo "Check cannot run against defined main branch!" >> "$GITHUB_OUTPUT"
-    exit 1
-  fi
+  [[ -n "${GITHUB_HEAD_REF}" ]] || die "Must be run on pull_request or pull_request_target triggers only!"
 
-  commit_range="$(git merge-base "$GITHUB_BASE_REF" "$GITHUB_HEAD_REF")..$GITHUB_HEAD_REF"
+  [ ! "$GITHUB_BASE_REF" = "$2" ] || die "Check cannot run against defined main branch!"
+
+  commit_range="$GITHUB_BASE_REF".."$GITHUB_HEAD_REF"
   echo "Checking range $commit_range which contains $(git log --oneline "$commit_range" | wc -l) commits" >> "$GITHUB_OUTPUT"
-  commits=$(git log --format=format:%H "$commit_range")
 
-  dirty=0
-  check_branch=$2
-
-  for line in $commits; do
+  git log --format=format:%H "$commit_range" | while read -r line; do
     echo -n "$(git log --format=format:"Checking %h %s..." "$line^!")" >> "$GITHUB_OUTPUT"
-    if [ "$1" = "true" ]; then
-      do_reference_check
+    if [ "$ref_check" = "true" ]; then
+      ret=$( do_reference_check "$line" "$check_branch" )
     else
-      do_subject_check
+      ret=$( do_subject_check "$line" "$check_branch" )
     fi
+    DIRTY_PR=$(( ret + DIRTY_PR ))
   done
 
-  exit $dirty
+  [ $DIRTY_PR = 0 ] || exit 1;
 }
 
 main "$@"
